7b48238deb20a2ef6833c8e32eb3a631c47b3982
Oscar Gomez Toro <oscarstiven87@gmail.com>
2019-09-18 15:50:53 +0000

    Task 4 for project 0x00-hello_world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+ *Prints exactly "Programming is like building a multilingual puzzle, followed
+ *by a new line
+ */
+
+int main()
+{
+	char cisfun[]="\"Programming is like building a multilingual puzzle";
+	puts (cisfun);
+	return (0);
+}
