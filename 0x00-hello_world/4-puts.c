21a27fee259360bb29b3d34543a65ea6d92f57b5
Oscar Gomez Toro <oscarstiven87@gmail.com>
2019-09-18 16:45:17 +0000

    Format fixes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
 /*
- *Prints exactly "Programming is like building a multilingual puzzle, followed
- *by a new line
- */
-
-int main()
+ *Prints exactly "Programming is like building a multilingual puzzle,
+ *followedby a new line
+*/
+int main(void)
 {
-	char cisfun[]="\"Programming is like building a multilingual puzzle";
-	puts (cisfun);
+	char cisfun[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(cisfun);
+
 	return (0);
 }
