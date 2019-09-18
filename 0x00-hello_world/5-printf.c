d4ab8465e862298960fdfc7147289104e21aa72b
Oscar Gomez Toro <oscarstiven87@gmail.com>
2019-09-18 16:48:19 +0000

    Format fixes for task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/*
+ * Prints exactly with proper grammar, but the outcome is a piece of art,
+ *followed by a new line
+ */
+int main(void)
+{
+	char str[] = "with proper grammar, but the outcome is a piece of art,";
+
+	printf("%s\n", str);
+
+	return (0);
+}
