63512fd90df8998d6a5bb3ff7742bd3dec5ee429
Oscar Gomez Toro <oscarstiven87@gmail.com>
2019-09-18 17:27:37 +0000

    Format fixes for task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
 /*
- * Prints exactly with proper grammar, but the outcome is a piece of art,
+ *Prints exactly with proper grammar, but the outcome is a piece of art,
  *followed by a new line
+ */
+
+/**
+ * main - Prints exactly "with proper grammar, but the outcome is
+ *a piece of art," followed by a new line
+ * Return: 0 when everthing is ok
  */
 int main(void)
 {
