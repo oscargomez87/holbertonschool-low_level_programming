2b206019cb6b485bf64c5bd0562d94ac02bcc075
Oscar Gomez Toro <oscarstiven87@gmail.com>
2019-09-19 23:51:36 +0000

    Task 7 for project 0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/*
+ *
+ */
+
+/**
+ * main - Prints the lowercase alphabet in reverse
+ *
+ * Return: 0 when successful
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 122; i > 96; i--)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
