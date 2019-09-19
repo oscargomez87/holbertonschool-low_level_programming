ee94f6b325449ce6a8feb58641bec2b54d87c6f9
Oscar Gomez Toro <oscarstiven87@gmail.com>
2019-09-19 15:16:19 +0000

    Task 1 for project 0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - assign a random number to the variable n each time it is executed,
+ * evaluate if n is greater than 5, 0 or less than 6 and print a message.
+ *
+ * Return: o when succesful
+ */
+int main(void)
+{
+	int n;
+	int last;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	last = n % 10;
+	if (n > 5)
+		printf("%s %i %s %i %s\n", "Last digit of", n, "is", last,
+		       "and is greater than 5");
+	else if (n == 0)
+		printf("%s %i %s %i %s\n", "Last digit of", n, "is", last,
+		       "and is 0");
+	else
+		printf("%s %i %s %i %s\n", "Last digit of", n, "is", last,
+		       "and is less than 6 and not 0");
+	return (0);
+}
