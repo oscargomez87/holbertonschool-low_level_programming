309b8512bb5b6f4591f0b2d3d7da724c1dcb4a09
Oscar Gomez Toro <oscarstiven87@gmail.com>
2019-09-20 03:32:57 +0000

    Task 102 project 0x01-variables_if_else_while

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -4,40 +4,40 @@
  */
 
 /**
- * main - prints the numbers from 00 to 99
+ * main - prints all possible combinations of two two-digit numbers
  *
  * Return: 0 when successful
  */
 int main(void)
 {
-	int i, j, k, l;
+int i, j, k, l;
 
-	for (i = 48; i < 58; i++)
+for (i = 48; i < 58; i++)
+{
+	for (j = 48; j < 58; j++)
 	{
-		for (j = 48; j < 58; j++)
+		for (k = 48; k < 58; k++)
 		{
-			for (k = 48; k < 58; k++)
+			for (l = 48; l < 58; l++)
 			{
-				for (l = 48; l < 58; l++)
+				if ((i < k) || (i == k && j < l))
 				{
-					if ((i < k) || (i == k && j < l))
+					putchar(i);
+					putchar(j);
+					putchar(' ');
+					putchar(k);
+					putchar(l);
+					if (!(i == 57 && j == 56 && k == 57
+					      && l == 57))
 					{
-						putchar(i);
-						putchar(j);
+						putchar(',');
 						putchar(' ');
-						putchar(k);
-						putchar(l);
-						if (!(i == 57 && j == 56
-						      && k == 57 && l == 57))
-						{
-							putchar(',');
-							putchar(' ');
-						}
 					}
 				}
 			}
 		}
 	}
-	putchar('\n');
-	return (0);
+}
+putchar('\n');
+return (0);
 }
