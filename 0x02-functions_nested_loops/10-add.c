f25fd4b6b9c7f725b29370385076dd22e0fed7af
Oscar Gomez Toro <oscarstiven87@gmail.com>
2019-09-24 18:45:31 +0000

    Task 10 for project 0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+/**
+ * add - adds two integers
+ * @n1: first integer value
+ * @n2: second integer value
+ * Return: adds two integers and returns the result.
+ */
+int add(int n1, int n2)
+{
+	int a = n1;
+	int b = n2;
+
+	return (a + b);
+}
