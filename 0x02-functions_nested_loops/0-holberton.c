19970ce3db205ef8fdf791a85533e7e0bd1b0c48
Oscar Gomez Toro <oscarstiven87@gmail.com>
2019-09-24 14:28:19 +0000

    Fix for task 0 for project 0x02-functions_nested_loops

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -11,9 +11,10 @@
 int main(void)
 {
 	char str[] = "Holberton";
+	int stop = sizeof(str);
 	int i;
 
-	for (i = 0; i < (int)sizeof(str); i++)
+	for (i = 0; i < stop; i++)
 		_putchar(str[i]);
 	_putchar('\n');
 	return (0);
