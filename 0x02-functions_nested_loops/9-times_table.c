c9e3e75aa7e8e85c32832e85d96abd7fcd65da5e
Oscar Gomez Toro <oscarstiven87@gmail.com>
2019-09-24 18:38:03 +0000

    Fix for task 9 project 0x02-functions_nested_loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,31 +13,28 @@ void times_table(void)
 		for (j = 0; j < 10; j++)
 		{
 			n = i * j;
-			if ( i == 0 && j != 0)
+			if (i == 0 && j != 0)
 			{
 				if (i > 0)
 				{
-					_putchar(n/10 + '0');
+					_putchar(n / 10 + '0');
 				}
 				else
 				{
 					_putchar(' ');
-					_putchar(n/10 + '0');
+					_putchar(n / 10 + '0');
 				}
-			} else if (j == 0)
+			} else if (j == 0 || (i > 0 && j == 0))
 			{
-				_putchar(n/10 + '0');
-			} else if (i > 0 && j == 0 )
+				_putchar(n / 10 + '0');
+			} else if (i > 0 && j >  0 && n / 10 != 0)
 			{
-				_putchar(n/10 + '0');
-			} else if (i > 0 && j >  0 && n/10 != 0)
-			{
-				_putchar(n/10 + '0');
-				_putchar(n%10 + '0');
-			} else if (i > 0 && j >  0 && n/10 == 0)
+				_putchar(n / 10 + '0');
+				_putchar(n % 10 + '0');
+			} else if (i > 0 && j >  0 && n / 10 == 0)
 			{
 				_putchar(' ');
-				_putchar(n%10 + '0');
+				_putchar(n % 10 + '0');
 			}
 			if (j != 9)
 			{
