aa4de7a9eac69a15493bc3e85ecec5e06d5f6def
Oscar Gomez Toro <oscarstiven87@gmail.com>
2019-09-24 14:36:50 +0000

    Removed loop

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -10,12 +10,15 @@
  */
 int main(void)
 {
-	char str[] = "Holberton";
-	int stop = sizeof(str);
-	int i;
-
-	for (i = 0; i < stop; i++)
-		_putchar(str[i]);
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
 	_putchar('\n');
 	return (0);
 }
