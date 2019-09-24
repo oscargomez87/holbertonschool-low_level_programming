90f52e4573f54e404f5eff127fdcdd5c9270dd57
Oscar Gomez Toro <oscarstiven87@gmail.com>
2019-09-24 15:50:57 +0000

    Task 8 for project 0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer,
+ * starting from 00:00 to 23:59
+ */
+void jack_bauer(void)
+{
+	int h1, h2, m1, m2;
+
+	for (h1 = 0; h1 < 3; h1++)
+	{
+		for (h2 = 0; h2 < 10; h2++)
+		{
+			for (m1 = 0; m1 < 6; m1++)
+			{
+				for (m2 = 0; m2 < 10; m2++)
+				{
+					if (h1 == 2 && h2 > 3)
+					else
+					{
+						_putchar(h1 + '0');
+						_putchar(h2 + '0');
+						_putchar(':');
+						_putchar(m1 + '0');
+						_putchar(m2 + '0');
+						_putchar('\n');
+					}
+				}
+			}
+		}
+	}
+}
