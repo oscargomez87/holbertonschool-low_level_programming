e046b5c98ae136d247f1e9da6c486e29a1c6d65a
Oscar Gomez Toro <oscarstiven87@gmail.com>
2019-09-24 15:35:46 +0000

    Fix for task 7 project 0x02-functions_nested_loops

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,15 +2,15 @@
 /**
  * print_last_digit - prints the last digit of a number
  * @n: number to evaluate
- * Return: 0 when successful
+ * Return: last digit when successful
  */
 int print_last_digit(int n)
 {
 	int m = n;
 
+	m = m % 10;
 	if (m < 0)
 		m = m * (-1);
-	m = m % 10;
 	_putchar(m + '0');
 	return (m);
 }
