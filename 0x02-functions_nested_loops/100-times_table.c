314739dcf18a71ddf152593eddf303014a6082e2
oscargomez87 <oscarstiven87@gmail.com>
2019-09-25 02:11:57 +0000

    Task 100 for project 0x02-functions_nested_loops

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,39 +1,41 @@
 #include "holberton.h"
 
 /**
- * times_table - prints the 9 times table, starting with 0.
- *
+ * print_times_table - prints n times the table, starting with 0.
+ * @n: number to process
  */
 void print_times_table(int n)
 {
 	int i, j, m;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i <= n; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
 			m = i * j;
-			if (i == 0 && j != 0)
+			if (i == 0 && j > 0)
 			{
-				if (i > 0)
-				{
-					_putchar(m / 10 + '0');
-				}
-				else
-				{
-					_putchar(' ');
-					_putchar(m / 10 + '0');
-				}
-			} else if (j == 0 || (i > 0 && j == 0))
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(m / 10 + '0');
+			}
+			else if (j == 0 || (i > 0 && j == 0))
 			{
 				_putchar(m / 10 + '0');
-			} else if (i > 0 && j >  0 && m / 10 != 0)
+			} else if (i > 0 && j >  0 && m / 100 != 0)
 			{
-				_putchar(m / 10 + '0');
+				_putchar(m / 100 + '0');
+				_putchar((m / 10) % 10 + '0');
 				_putchar(m % 10 + '0');
 			} else if (i > 0 && j >  0 && m / 10 == 0)
 			{
 				_putchar(' ');
+				_putchar(' ');
+				_putchar(m % 10 + '0');
+			} else if (i > 0 && j >  0 && (m / 10) % 10 != 0)
+			{
+				_putchar(' ');
+				_putchar(m / 10 + '0');
 				_putchar(m % 10 + '0');
 			}
 			if (j != 9)
