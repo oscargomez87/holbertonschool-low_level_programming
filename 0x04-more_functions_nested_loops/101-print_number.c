52ab0fb10d9f893e12aca8e3b937561106e7b9ae
oscargomez87 <oscarstiven87@gmail.com>
2019-09-26 22:19:45 +0000

    betty fixes for task 12

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,53 @@
+#include "holberton.h"
+void print_negative(int n);
+void print_positive(int n);
+void print_zero(int n);
+
+/**
+ * print_number - prints an integer
+ * @n: integer to print
+ */
+void print_number(int n)
+{
+	print_zero(n);
+	if (n < 0)
+		print_negative(n);
+	else if (n > 0)
+		print_positive(n);
+}
+
+/**
+ * print_negative - transform negative to positive, then calls print_positive
+ * @n: integer to transform
+ */
+void print_negative(int n)
+{
+	_putchar('-');
+	n *= -1;
+	print_positive(n);
+}
+
+/**
+ * print_positive - prints positive integer
+ * @n: integer to print
+ */
+void print_positive(int n)
+{
+	if (n != 0)
+	{
+		print_positive(n / 10);
+		_putchar((n % 10) + '0');
+	}
+}
+
+/**
+ * print_zero - if the integer is equal to 0 prints 0
+ * @n: integer to print
+ */
+void print_zero(int n)
+{
+	if (n == 0)
+	{
+		_putchar('0');
+	}
+}
