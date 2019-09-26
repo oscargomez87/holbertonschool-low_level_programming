2ecc179e42ae081758be10a77144f335774cf7c6
oscargomez87 <oscarstiven87@gmail.com>
2019-09-26 17:00:38 +0000

    files for task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ *
+ *
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (!(i == 2 || i == 4))
+			_putchar(i + '0');
+	}
+	_putchar('\n');
+}
