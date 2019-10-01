a13ab3d6df35e3fce42d17d24483c6f8d8119856
oscargomez87 <oscarstiven87@gmail.com>
2019-10-01 17:07:35 +0000

    Handles odd arrays

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdio.h>
 
 /**
  * puts_half - prints half of a string, followed by a new line
@@ -8,7 +9,7 @@ void puts_half(char *str)
 {
 	int i = 0, j;
 
-	while (*(str + i) != '\0')
+	while (str[i])
 		i++;
 	if (i % 2 == 0)
 	{
@@ -17,7 +18,7 @@ void puts_half(char *str)
 		_putchar('\n');
 	} else
 	{
-		for (j = (i - 1) / 2; j < (i - 1); j++)
+		for (j = ((i - 1) / 2) + 1; j < i; j++)
 			_putchar(*(str + j));
 		_putchar('\n');
 	}
