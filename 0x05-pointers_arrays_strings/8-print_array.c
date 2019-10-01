1ea883285cc5320b306306c05266c477e5d192c5
oscargomez87 <oscarstiven87@gmail.com>
2019-10-01 18:08:34 +0000

    Add description to task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+
+/**
+ * print_array - print the first n elements in an array
+ * @a: array
+ * @n: number of elements to print
+ */
 void print_array(int *a, int n)
 {
 	int i;
