7c158d5c4909e1d05e79c6ce3e7fcb2d14b4085a
oscargomez87 <oscarstiven87@gmail.com>
2019-10-01 19:47:41 +0000

    changed conditional and new line location

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,9 +12,8 @@ void print_array(int *a, int n)
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", *(a + i));
-		if ((i + 1) != n)
+		if (i < n - 1)
 			printf(", ");
-		else
-			printf("\n");
 	}
+	printf("\n");
 }
