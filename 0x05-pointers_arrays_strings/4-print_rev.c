ccb217501de2425831304244bd2356b9f48a2c89
oscargomez87 <oscarstiven87@gmail.com>
2019-10-01 14:32:12 +0000

    Format fixex

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,12 +10,12 @@ void print_rev(char *s)
 	int i = 0;
 
         while (s[i])
-        {
-                i++;
-        }
-        for (; i >= 0; i--)
-        {
-                _putchar(s[i]);
-        }
-        _putchar('\n');
+	{
+		i++;
+	}
+	for (; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
 }
