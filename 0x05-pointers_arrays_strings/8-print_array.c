6f8d0cc18a67bd712fa4cb8896faad109958fd49
oscargomez87 <oscarstiven87@gmail.com>
2019-10-01 19:26:45 +0000

    change array display for pointer arithmetic

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,8 +12,8 @@ void print_array(int *a, int n)
 	for (i = 0; i < n; i++)
 	{
 		if ((i + 1) != n)
-			printf("%d, ", a[i]);
+			printf("%d, ", *(a + i));
 		else
-			printf("%d\n", a[i]);
+			printf("%d\n", *(a + i));
 	}
 }
