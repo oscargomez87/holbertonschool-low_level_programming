b04a4e9dd15b2ded43e64740821dc8d86b842edb
oscargomez87 <oscarstiven87@gmail.com>
2019-10-03 19:49:17 +0000

    task 6 - string to uppercase

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,44 @@
+/**
+ * cap_string - capitalizes all words of a string.
+ * @s: string
+ * Return: Capitalized string
+ */
+char *cap_string(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		switch (s[i])
+		{
+		case 32:
+		case 13:
+		case 10:
+			if (s[i + 1] != '\0')
+			{
+				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+					s[i + 1] = s[i + 1] - 32;
+			}
+			break;
+		case 9:
+		case 44:
+		case 59:
+		case 46:
+		case 33:
+		case 63:
+		case 34:
+		case 40:
+		case 41:
+		case 123:
+		case 125:
+			if (s[i + 1] != '\0')
+			{
+				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+					s[i + 1] = s[i + 1] - 32;
+			}
+			break;
+		}
+		i++;
+	}
+	return (s);
+}
