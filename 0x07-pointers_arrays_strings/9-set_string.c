a05db2fbfc3c57f1461eb11a1f5c5fa4064f99d2
oscargomez87 <oscarstiven87@gmail.com>
2019-10-07 22:54:19 +0000

    Task 8

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,9 @@
+/**
+ * set_string - sets the value of a pointer to a char.
+ * @s: double pointer var to set
+ * @to: value to change s into
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
