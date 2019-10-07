e9753109fc7a40b967c8cdec7873fec57f96e3bc
oscargomez87 <oscarstiven87@gmail.com>
2019-10-07 14:42:42 +0000

    Project 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+/**
+ * _strchr - locates a character in a string.
+ * @s: string to search in.
+ * @c: char to locate.
+ * Return: pointer to the character or null if its not found.
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+	char *p;
+
+	while (s[i])
+	{
+		if  (s[i] == c)
+		{
+			if (p == '\0')
+				p = &s[i];
+		}
+		i++;
+	}
+	return (p);
+}
