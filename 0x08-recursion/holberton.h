0e1a4e302aacfca87dbe1b087dd131939bb54fef
oscargomez87 <oscarstiven87@gmail.com>
2019-10-09 16:45:36 +0000

    Add prototype for task 4

--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -2,4 +2,5 @@ void _puts_recursion(char *);
 int _putchar(char);
 void _print_rev_recursion(char *);
 int _strlen_recursion(char *);
-int factorial(int n);
+int factorial(int);
+int _pow_recursion(int, int);
