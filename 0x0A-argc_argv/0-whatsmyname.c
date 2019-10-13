a1de615a658d8a5208ced8deb2eaba396c41ed90
oscargomez87 <oscarstiven87@gmail.com>
2019-10-13 13:22:07 +0000

    Format fixes

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
-int main(int argc __attribute__((unused)), char *argv[] __attribute__((unused)))
+
+/**
+ * main - prints the program name
+ * @argc: size of argument
+ * @argv: argument elements
+ * Return: 0 if sucessful
+ */
+int main(int argc __attribute__((unused)),
+char *argv[] __attribute__((unused)))
 {
 	printf("%s\n", argv[0]);
 	return (0);
