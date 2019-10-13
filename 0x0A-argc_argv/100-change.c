903adf0b6daf2edad490eea7d3804e37efe6ee55
oscargomez87 <oscarstiven87@gmail.com>
2019-10-13 18:24:00 +0000

    Fix compilation error, wrogn end block

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -23,23 +23,22 @@ int main(int argc, char *argv[])
 	if (num < 0)
 		printf("0\n");
 	else
+	{
+		while (num > 0)
 		{
-			while (num > 0)
-			{
-				if (num % 25 == 0)
-					num -= 25;
-				else if (num % 10 == 0)
-					num -= 10;
-				else if (num % 5 == 0)
-					num -= 5;
-				else if (num % 2 == 0)
-					num -= 2;
-				else
-					num -= 1;
-				sum++;
-			}
-			printf("%d\n", sum);
+			if (num % 25 == 0)
+				num -= 25;
+			else if (num % 10 == 0)
+				num -= 10;
+			else if (num % 5 == 0)
+				num -= 5;
+			else if (num % 2 == 0)
+				num -= 2;
+			else
+				num -= 1;
+			sum++;
 		}
+		printf("%d\n", sum);
 	}
 	return (0);
 }
