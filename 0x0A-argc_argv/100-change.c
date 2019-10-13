3ce386829607956f42e33925a6f7b92f569cad57
oscargomez87 <oscarstiven87@gmail.com>
2019-10-13 18:13:06 +0000

    Remove string to int check

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -12,64 +12,31 @@ int _checki(char *);
  */
 int main(int argc, char *argv[])
 {
-	long num = 0;
-	int sum = 0;
+	int num, sum = 0;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	if (_checki(argv[1]))
+	num = atoi(argv[1]);
+	if (num != -1)
 	{
-		num = atoi(argv[1]);
-		if (num != -1)
+		while (num > 0)
 		{
-			while (num > 0)
-			{
-				if (num % 25 == 0)
-				{
-					sum += 1;
-					num -= 25;
-				} else if (num % 10 == 0)
-				{
-					sum += 1;
-					num -= 10;
-				} else if (num % 5 == 0)
-				{
-					sum += 1;
-					num -= 5;
-				} else if (num % 2 == 0)
-				{
-					sum += 1;
-					num -= 2;
-				} else
-				{
-					sum += 1;
-					num -= 1;
-				}
-			}
+			if (num % 25 == 0)
+				num -= 25;
+			else if (num % 10 == 0)
+				num -= 10;
+			else if (num % 5 == 0)
+				num -= 5;
+			else if (num % 2 == 0)
+				num -= 2;
+			else
+				num -= 1;
+			sum++;
 		}
 		printf("%d\n", sum);
-	} else
-		printf("0\n");
-	return (0);
-}
-
-/**
- * _checki - checks if string can be converted to int
- * @s: string to check
- * Return: 0 if argument is not number, 1 otherwise
- */
-int _checki(char *s)
-{
-	int i = 1;
-
-	if (*s)
-	{
-		if (*s < (0 + '0') || *s > (9 + '0'))
-			return (0);
-		i = _checki(s + 1);
 	}
-	return (i);
+	return (0);
 }
