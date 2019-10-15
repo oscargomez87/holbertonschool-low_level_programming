d50678acad0d394ffa16869815a87bdb6d95dd0a
oscargomez87 <oscarstiven87@gmail.com>
2019-10-15 17:56:05 +0000

    Test if malloc fails

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -10,12 +10,11 @@ char *_strdup(char *str)
 	unsigned int i, size = 0;
 	char *p;
 
-	if (*str == '\0')
-		return ('\0');
 	while (str[size])
 		size++;
-	str = str - size;
 	p = malloc(size * sizeof(char));
+	if (!p)
+		return (NULL);
 	for (i = 0; i < size; i++)
 		p[i] = str[i];
 	p[i] = '\0';
