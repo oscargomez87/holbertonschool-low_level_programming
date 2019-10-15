d39399534de337a4ba8f4057b57efacc910cb4ea
oscargomez87 <oscarstiven87@gmail.com>
2019-10-15 17:51:33 +0000

    Handle no memory error

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,6 +14,8 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return ('\0');
 	p = malloc(size * sizeof(char));
+	if (!p)
+		return NULL;
 	for (i = 0; i < size; i++)
 		p[i] = c;
 	p[i] = '\0';
