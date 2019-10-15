9465b0b6ac2444c5011ab1cb0a6897dd83fbef35
oscargomez87 <oscarstiven87@gmail.com>
2019-10-15 20:00:40 +0000

    Fix wrong memory allocation

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -19,7 +19,7 @@ char *str_concat(char *s1, char *s2)
 		size1++;
 	while (s2[size2])
 		size2++;
-	p = malloc((size1 + size2) * sizeof(char));
+	p = malloc((size1 + size2) * sizeof(char) + 1);
 	if (!p)
 		return (NULL);
 	for (i = 0; i < (size1 + size2); i++)
