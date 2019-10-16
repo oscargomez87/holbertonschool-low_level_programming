cfc4e9645abc17ef323ba2a0885755869f55bece
oscargomez87 <oscarstiven87@gmail.com>
2019-10-16 00:56:52 +0000

    checker test

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -27,10 +27,10 @@ int **alloc_grid(int width, int height)
 	{
 		for (j = 0; j <= width; j++)
 		{
-			if (j == width)
+			/*(if (j == width)
 				p[i][j] = '\0';
-			else
-				p[i][j] = 0;
+				else*/
+			p[i][j] = 0;
 		}
 	}
 	return (p);
