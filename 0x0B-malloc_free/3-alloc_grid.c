3a1e942aa85ec09bd97b38f3ae5694dec17af329
oscargomez87 <oscarstiven87@gmail.com>
2019-10-16 01:01:27 +0000

    checker test

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -13,7 +13,7 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	p = malloc(height * sizeof(int *) + 1);
+	p = malloc(height * sizeof(int *));
 	if (!p)
 		return (NULL);
 	for (i = 0; i < height; i++)
