735e980a1acf058a7d8da79d4fb661fdc7a351a7
oscargomez87 <oscarstiven87@gmail.com>
2019-10-17 19:48:35 +0000

    Add prototyppe for task 3

--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -1,4 +1,5 @@
 int _putchar(char);
 void *malloc_checked(unsigned int);
 char *string_nconcat(char *, char *, unsigned int);
-void *_calloc(unsigned int nmemb, unsigned int size);
+void *_calloc(unsigned int, unsigned int);
+int *array_range(int, int);
