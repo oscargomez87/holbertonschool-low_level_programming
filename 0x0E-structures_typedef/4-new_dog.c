9775fdab14b121716d2f57211f5ce1f74f4ef68f
oscargomez87 <oscarstiven87@gmail.com>
2019-10-21 20:22:47 +0000

    Add comments to function, also format fixes

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,14 +1,22 @@
 #include <stdlib.h>
 #include "dog.h"
+
+/**
+ * new_dog - creates a new dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ * Return: NULL if memory allocation failes, otherwise a pointer to a new dog
+ */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	struct dog *ndog;
 
 	ndog = malloc(sizeof(struct dog));
 	if (ndog == NULL)
-		return NULL;
+		return (NULL);
 	ndog->name = name;
-	ndog-> age = age;
+	ndog->age = age;
 	ndog->owner = owner;
 	return (ndog);
 }
