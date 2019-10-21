895f55c25fbeb3fdee4326e844377b0b20ca2616
oscargomez87 <oscarstiven87@gmail.com>
2019-10-21 21:35:58 +0000

    Function that frees dog and main test file

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - frees memory allocated for dog structure
+ * @d: structure to free
+ */
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
