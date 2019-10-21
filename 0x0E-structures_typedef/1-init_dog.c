6dec460ab1f2b3ebf45da2d591d90ad15df3f9fc
oscargomez87 <oscarstiven87@gmail.com>
2019-10-21 18:00:29 +0000

    Remove local pointer and made use of pointer passed as parameter

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -9,10 +9,7 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	struct dog *p;
-
-	p = d;
-	p->name = name;
-	p->age = age;
-	p->owner = owner;
+	d->name =name;
+	d->age = age;
+	d->owner = owner;
 }
