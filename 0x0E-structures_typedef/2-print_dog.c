3b0557d1f0c5f991765198681098109cc5a55556
oscargomez87 <oscarstiven87@gmail.com>
2019-10-21 18:45:11 +0000

    Function that prints the struct dog with a main test case

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "dog.h"
+/**
+ * print_dog - prints dog info
+ * @d: pointer to structure with dog info
+ */
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		if (!d->name)
+			printf("Name: %s\n", d->name);
+		else
+			printf("Name: (nil)\n");
+		if ((int)d->age > 0)
+                        printf("Age: %f\n", d->age);
+		else
+			printf("Age: (nil)\n");
+		if (!d->owner)
+                        printf("Owner: %s\n", d->owner);
+                else
+                        printf("Owner: (nil)\n");
+	}
+}
