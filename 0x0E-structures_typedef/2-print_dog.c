c6034f8dc535cc8bcc8514bcad905b19d6171e17
oscargomez87 <oscarstiven87@gmail.com>
2019-10-21 19:24:30 +0000

    Fix NULL eval

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,7 +9,7 @@ void print_dog(struct dog *d)
 {
 	if (d != NULL)
 	{
-		if (!d->name)
+		if (d->name != NULL)
 			printf("Name: %s\n", d->name);
 		else
 			printf("Name: (nil)\n");
@@ -17,7 +17,7 @@ void print_dog(struct dog *d)
 			printf("Age: %f\n", d->age);
 		else
 			printf("Age: (nil)\n");
-		if (!d->owner)
+		if (d->owner != NULL)
 			printf("Owner: %s\n", d->owner);
 		else
 			printf("Owner: (nil)\n");
