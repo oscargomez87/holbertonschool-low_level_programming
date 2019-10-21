1759dbc4ee6213c3d246c984ffa19aef6eb7e1da
oscargomez87 <oscarstiven87@gmail.com>
2019-10-21 19:47:09 +0000

    New typedef for dog structure and a test program

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,12 +9,12 @@ void _putchar(int);
  * @age: dog's age
  * @owner: dog's owner
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 void init_dog(struct dog *, char *, float, char *);
 void print_dog(struct dog *);
 
