fc583921c0f684d6a82f6370ac330a5a19111a8c
oscargomez87 <oscarstiven87@gmail.com>
2019-10-21 17:16:43 +0000

    Create var p, define as struct dog and assign d

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -9,7 +9,10 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	struct dog *p;
+
+	p = d;
+	p->name = name;
+	p->age = age;
+	p->owner = owner;
 }
