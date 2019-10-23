f6bfed0cafb26ee4b23e297cf96d0913df741007
oscargomez87 <oscarstiven87@gmail.com>
2019-10-23 17:22:31 +0000

    Wrong directory, moved to 0x0F-function_pointers

--- function_pointers.h
+++
@@ -1,8 +0,0 @@
-#ifndef FUNCTION_POINTERS_H
-#define FUNCTION_POINTERS_H
-
-void print_name(char *, void (*)(char *));
-void array_iterator(int *, size_t, void (*)(int));
-int int_index(int *, int, int (*)(int));
-
-#endif
