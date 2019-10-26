4dd824c01d3ae04b22d4b64dac09607da513b9fb
oscargomez87 <oscarstiven87@gmail.com>
2019-10-26 02:33:08 +0000

    Added prototype for function 2-print_strings.c

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,5 +4,6 @@
 #include <stdarg.h>
 int sum_them_all(const unsigned int, ...);
 void print_numbers(const char *, const unsigned int, ...);
+void print_strings(const char *, const unsigned int, ...);
 
 #endif
