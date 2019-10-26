dc4255129323dd5a417f6c24d17459902eef423b
oscargomez87 <oscarstiven87@gmail.com>
2019-10-26 02:05:59 +0000

    Added function that sum all parameters recieved and a main test file

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,19 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - Adds all parameters received
+ * @n: Number of arguments
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list valist;
+	int i, sum = 0;
+
+	if (n == 0)
+		return (0);
+	va_start(valist, n);
+	for (i = 0; i < (int)n; i++)
+		sum += va_arg(valist, int);
+	va_end(valist);
+	return (sum);
+}
