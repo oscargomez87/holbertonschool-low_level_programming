74da158ecb63d205c01db2ed3a630a92e1f7c08e
oscargomez87 <oscarstiven87@gmail.com>
2019-10-31 22:02:55 +0000

    Betty compliance

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,11 +1,11 @@
 #include "lists.h"
 
 /**
+ * add_node_end - adds a node to the end of a list
  *
- *
- *
- *
- *
+ * @head: root of list
+ * @str: string to store in node
+ * Return: the address of the root of the list
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
