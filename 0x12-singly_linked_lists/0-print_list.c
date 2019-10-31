f615b858e9b283e0ad266fe34dfe8d4a76d874ad
oscargomez87 <oscarstiven87@gmail.com>
2019-10-31 18:18:09 +0000

    Beety compliance

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,13 +9,10 @@
 size_t print_list(const list_t *h)
 {
 	if (h->str != NULL)
-	{
-		printf("[%d] ", h->len);
-		printf("%s\n", h->str);
-	}
+		printf("[%d] %s\n", h->len, h->str);
 	else
 		printf("[0] %p\n", h->str);
-	if (h ->next != NULL)
+	if (h->next != NULL)
 		return (1 + print_list(h = h->next));
-	return 1;
+	return (1);
 }
