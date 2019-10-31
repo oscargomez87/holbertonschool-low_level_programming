cfc40fdbb70cec8df607486e4d82febae0f4c1bd
oscargomez87 <oscarstiven87@gmail.com>
2019-10-31 21:40:25 +0000

    Stopped checking if str is null at start and instead check if is null after strdup

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -13,12 +13,15 @@ list_t *add_node(list_t **head, const char *str)
 	char *s;
 	list_t *new;
 
-	if (str == NULL)
-		return (NULL);
-	s = strdup(str);
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
+	s = strdup(str);
+	if (s == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
 	if (*head == NULL)
 	{
 		new->str = s;
