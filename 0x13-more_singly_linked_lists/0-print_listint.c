77e7215806a8c2599213fa00a40e21c5e1538c67
oscargomez87 <oscarstiven87@gmail.com>
2019-11-04 13:46:07 +0000

    Comments describing the program, inputs and outputs

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,5 +1,12 @@
 #include "lists.h"
 
+/**
+ * print_listint - Prints all the elements of a listint_t list and
+ * returns the number of nodes
+ *
+ * @h: list of type listint_t to print
+ * Return: Number of nodes
+ */
 size_t print_listint(const listint_t *h)
 {
 	if (h == NULL)
