7702e77586e00e0dbcc4749c7ba32dcd07a11db3
oscargomez87 <oscarstiven87@gmail.com>
2019-11-04 13:43:54 +0000

    Program that prints all the elements of a listint_t list and returns the number of nodes

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,7 +1,9 @@
+#include "lists.h"
+
 size_t print_listint(const listint_t *h)
 {
 	if (h == NULL)
 		return (0);
-	printf(h->n);
-	return (1 + print_listint(h->next);
+	printf("%d\n", h->n);
+	return (1 + print_listint(h->next));
 }
