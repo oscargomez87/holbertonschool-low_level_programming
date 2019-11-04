fb21d157336073cf88667b3cc5b03c2f94ef3bda
oscargomez87 <oscarstiven87@gmail.com>
2019-11-04 20:07:51 +0000

    Function that frees memory used by linked list and sets head to NULL

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * pop_listint2 - Frees memory used by linked list and sets
+ * head to NULL.
+ *
+ * @head: Start of list.
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	if (*head == NULL)
+		return;
+	while(*head != NULL)
+	{
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
+	}
+}
