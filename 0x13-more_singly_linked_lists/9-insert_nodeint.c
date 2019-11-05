a46410e1f2d6a7739565242f0cdbf4ccafed1db8
oscargomez87 <oscarstiven87@gmail.com>
2019-11-05 23:02:22 +0000

    removed duped code and changed the return value

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -17,10 +17,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new = malloc(sizeof(listint_t));
 	if (new == NULL || *head == NULL)
 		return (NULL);
+	new->n = n;
 	temp = *head;
 	if (idx == 0)
 	{
-		new->n = n;
 		new->next = *head;
 		*head = new;
 		return (*head);
@@ -37,8 +37,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		free(new);
 		return (NULL);
 	}
-	new->n = n;
 	new->next = temp->next;
 	temp->next = new;
-	return (temp->next);
+	return (new);
 }
