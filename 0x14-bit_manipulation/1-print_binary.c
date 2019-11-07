aec4e345bbfbc3208211d02c9e3767ccb76cbf75
oscargomez87 <oscarstiven87@gmail.com>
2019-11-07 19:41:19 +0000

    Format fixes, adding comment describing function print_nonzero

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,7 +1,8 @@
 #include "holberton.h"
 void print_nonzero(unsigned long int);
 /**
- * print_binary - Prints the binary representation of a number.
+ * print_binary - Prints the binary representation of a number
+ * if the number is equal to 0, otherwise call print_nonzero
  *
  * @n: number to print binary representation
  */
@@ -15,11 +16,16 @@ void print_binary(unsigned long int n)
 	print_nonzero(n);
 }
 
+/**
+ * print_nonzero - Prints the binary representation of a number bigger than 0
+ *
+ * @n: number to print binary representation
+ */
 void print_nonzero(unsigned long int n)
 {
 	if (n > 0)
 	{
 		print_nonzero(n >> 1);
 		_putchar((n & 1) + '0');
-        }
+	}
 }
