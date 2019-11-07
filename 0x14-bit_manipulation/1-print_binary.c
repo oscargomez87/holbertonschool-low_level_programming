87ac9dc6f9eb8643d0c6b91b070fbf2f2930209b
oscargomez87 <oscarstiven87@gmail.com>
2019-11-07 19:38:18 +0000

    Removed use of array, create new recursive function to handle !0

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,21 +1,25 @@
 #include "holberton.h"
-
+void print_nonzero(unsigned long int);
+/**
+ * print_binary - Prints the binary representation of a number.
+ *
+ * @n: number to print binary representation
+ */
 void print_binary(unsigned long int n)
 {
-	char s[255];
-	int i = 0;
 	if (n == 0)
+	{
 		_putchar('0');
-	while (n > 0)
-	{
-		s[i] = ((n & 1) == 0 ? '0' : '1');
-		n >>= 1;
-		i++;
+		return;
 	}
-	s[i] = '\0';
-	while (i >= 0)
+	print_nonzero(n);
+}
+
+void print_nonzero(unsigned long int n)
+{
+	if (n > 0)
 	{
-		_putchar(s[i]);
-		i--;
-	}
+		print_nonzero(n >> 1);
+		_putchar((n & 1) + '0');
+        }
 }
