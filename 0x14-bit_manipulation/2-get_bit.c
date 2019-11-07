038ce98178238447539ed17a3dff70d7e10ecb76
oscargomez87 <oscarstiven87@gmail.com>
2019-11-07 20:29:02 +0000

    reworked loop and the conditional that returns the bit in position index

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,9 +9,9 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (n == 0 && index > 0)
-		return (-1);
-	while (n > 0 && index > 0)
+	if (n == 0 && index == 0)
+		return (0);
+	while (index > 0)
 	{
 		n >>= 1;
 		index -= 1;
--- 0x14-bit_manipulation/2-main.c
+++ 0x14-bit_manipulation/2-main.c
@@ -16,5 +16,9 @@ int main(void)
 	printf("%d\n", n);
 	n = get_bit(1024, 0);
 	printf("%d\n", n);
+	n = get_bit(0, 15);
+	printf("%d\n", n);
+	n = get_bit(0, 0);
+	printf("%d\n", n);
 	return (0);
 }
