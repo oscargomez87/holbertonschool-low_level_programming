32743fe9def36d9c72cbd720fc3eb3fccd01d450
oscargomez87 <oscarstiven87@gmail.com>
2019-11-08 03:26:13 +0000

    Changed XOR for AND and ones complement operators

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -11,10 +11,6 @@ int clear_bit(unsigned long int *n, unsigned int index)
 {
 	if (index > 63)
 		return (-1);
-	if ((*n & (1 << index)))
-	{
-		*n = (*n ^ (1 << index));
-		return (1);
-	}
-	return (-1);
+	*n = (*n & ~(1 << index));
+	return (1);
 }
