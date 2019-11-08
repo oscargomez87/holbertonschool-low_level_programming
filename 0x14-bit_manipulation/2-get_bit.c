fb6c52e1fb3a170e939b803dccf77ba0b82b12ea
oscargomez87 <oscarstiven87@gmail.com>
2019-11-08 02:44:50 +0000

    Changed logic completly so it moves the mask all the way to the position needed, then retrieves that bit using & on the number and mask

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -11,12 +11,5 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	if (n == 0 && index > 0)
 		return (-1);
-	while (n > 0 && index > 0)
-	{
-		n >>= 1;
-		index -= 1;
-	}
-	if (index == 0 && n != 0)
-		return (n & 1);
-	return (-1);
+	return (n & (1 << index) ? 1 : 0);
 }
