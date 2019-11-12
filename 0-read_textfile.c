6a883b760c37aef20e5ee75109d663e830d7eb76
oscargomez87 <oscarstiven87@gmail.com>
2019-11-12 15:21:03 +0000

    Format fixes

--- 0-read_textfile.c
+++ 0-read_textfile.c
@@ -1,10 +1,12 @@
 #include "holberton.h"
 
 /**
+ * read_textfile - reads a text file and prints it to the
+ * POSIX standard output.
  *
- *
- *
- *
+ * @filename: path to file.
+ * @letters: number of letters to read from file.
+ * Return: number of bytes read.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
