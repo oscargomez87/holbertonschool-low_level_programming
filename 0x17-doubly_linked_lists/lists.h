c3b32fc159156f718dc50ef6396fa78527e789c5
oscargomez87 <oscarstiven87@gmail.com>
2019-12-15 18:56:47 +0000

    Added prototype for function add_dnodeint_end()

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -21,5 +21,6 @@ typedef struct dlistint_s
 } dlistint_t;
 size_t print_dlistint(const dlistint_t *);
 size_t dlistint_len(const dlistint_t *);
+dlistint_t *add_dnodeint_end(dlistint_t **, const int );
 
 #endif
