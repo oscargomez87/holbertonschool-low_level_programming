3bcab07c40193d64af88bfc1b24c2072c1f96d2a
oscargomez87 <oscarstiven87@gmail.com>
2019-12-15 19:19:39 +0000

    Task 2 - Adds a node to the start of a double linked list A main file to test the function

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - Adds a node to the start of a double linked list
+ *
+ * @head: double linked list
+ * @n: data to add to the new node
+ * Return: the address of the new node, NULL otherwise
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new, *temp;
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = NULL;
+	new->prev = NULL;
+	if (*head == NULL)
+	{
+		*head = new;
+		return (*head);
+	}
+	temp = *head;
+	while(temp->prev != NULL)
+		temp = temp->prev;
+	new->next = temp;
+	temp->prev = new;
+	*head = new;
+	return (*head);
+}
