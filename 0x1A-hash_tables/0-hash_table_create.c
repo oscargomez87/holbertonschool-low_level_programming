251de8db4c883b4d1445838f8fdd07973249b789
oscargomez87 <oscarstiven87@gmail.com>
2020-01-18 23:47:41 +0000

    First task for project (function that creates a hash table) Prototypes for tasks 0 and 1

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,20 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - Creates a hash table
+ *
+ * @size: size of hash table
+ * Return: NULL if creation fails, pointer to the hash table otherwise
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *htable;
+
+	htable = malloc(sizeof(size) + sizeof(hash_node_t **));
+	if (htable == NULL)
+		return (NULL);
+	htable->array = malloc(size * sizeof(hash_node_t *));
+	if (htable->array == NULL)
+	htable->size = size;
+	return (htable);
+}
