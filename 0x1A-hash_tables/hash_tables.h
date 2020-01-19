aa59c75dcde24818187a00009bd1d5f4bcfca0c9
oscargomez87 <oscarstiven87@gmail.com>
2020-01-19 00:01:07 +0000

    Fixed comment indentation

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -3,14 +3,14 @@
 
 #include <stdlib.h>
 
- /**
-	 * struct hash_node_s - Node of a hash table
-	 *
-	 * @key: The key, string
-	 * The key is unique in the HashTable
-	 * @value: The value corresponding to a key
-	 * @next: A pointer to the next node of the List
-	 */
+/**
+ * struct hash_node_s - Node of a hash table
+ *
+ * @key: The key, string
+ * The key is unique in the HashTable
+ * @value: The value corresponding to a key
+ * @next: A pointer to the next node of the List
+ */
 typedef struct hash_node_s
 {
 	char *key;
