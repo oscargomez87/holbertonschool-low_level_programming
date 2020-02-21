114fb329215898ff6e58c4b46505de75fdf29da4
oscargomez87 <oscarstiven87@gmail.com>
2020-02-21 18:53:59 -0500

    Reworked program, bad understanding of what was asked

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -6,19 +6,19 @@ def island_perimeter(grid):
     """Returns the perimeter"""
 
     perimeter = 0
-    x = 0
-    y = 0
     for idx1, val in enumerate(grid):
         for idx2 in range(len(val)):
             if grid[idx1][idx2] == 1:
-                if grid[idx1 - 1][idx2] == 1 and grid[idx1][idx2 + 1]:
-                    x += 1
-                    y += 1
-                    continue
-                if grid[idx1][idx2 + 1] == 1 or grid[idx1][idx2 - 1] == 1:
-                    x += 1
-                    continue
-                y += 1
-                break
-    perimeter = (2 * x) + (2 * y)
+                perimeter += 4
+                if grid[idx1 - 1][idx2] == 1:
+                    perimeter -= 1
+                if grid[idx1 + 1][idx2] == 1:
+                    perimeter -= 1
+                if grid[idx1][idx2 - 1] == 1:
+                    perimeter -= 1
+                if grid[idx1][idx2 + 1] == 1:
+                    perimeter -= 1
+                if grid[idx1][idx2 + 1] == 0:
+                    break
+        print("{} per: {}".format(idx1, perimeter))
     return perimeter
