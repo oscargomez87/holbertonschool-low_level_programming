cb622ed71a7c5447374a242cd583aa9069cdcac7
oscargomez87 <oscarstiven87@gmail.com>
2020-02-21 19:55:07 -0500

    Created vars for row and col

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -6,18 +6,22 @@ def island_perimeter(grid):
     """Returns the perimeter"""
 
     perimeter = 0
-    for idx1, val in enumerate(grid):
-        for idx2 in range(len(val)):
-            if grid[idx1][idx2] == 1:
+    row = len(grid)
+    col = len(grid[0])
+    for r in range(row):
+        for c in range(col):
+            if grid[r][c] == 1:
                 perimeter += 4
-                if grid[idx1 - 1][idx2] == 1:
-                    perimeter -= 1
-                if grid[idx1 + 1][idx2] == 1:
-                    perimeter -= 1
-                if grid[idx1][idx2 - 1] == 1:
-                    perimeter -= 1
-                if grid[idx1][idx2 + 1] == 1:
-                    perimeter -= 1
-                if grid[idx1][idx2 + 1] == 0:
-                    break
+                if r > 0:
+                    if grid[r - 1][c] == 1:
+                        perimeter -= 1
+                if r < (row - 1):
+                    if grid[r + 1][c] == 1:
+                       perimeter -= 1
+                if c > 0:
+                    if grid[r][c - 1] == 1:
+                       perimeter -= 1
+                if c < (col - 1):
+                    if grid[r][c + 1] == 1:
+                       perimeter -= 1
     return perimeter
