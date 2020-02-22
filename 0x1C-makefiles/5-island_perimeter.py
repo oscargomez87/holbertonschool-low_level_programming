b34c8cc0a943951f4ad3e31ea50e0b77f7f71cf9
oscargomez87 <oscarstiven87@gmail.com>
2020-02-21 19:56:52 -0500

    pep8 format fixes

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -17,11 +17,11 @@ def island_perimeter(grid):
                         perimeter -= 1
                 if r < (row - 1):
                     if grid[r + 1][c] == 1:
-                       perimeter -= 1
+                        perimeter -= 1
                 if c > 0:
                     if grid[r][c - 1] == 1:
-                       perimeter -= 1
+                        perimeter -= 1
                 if c < (col - 1):
                     if grid[r][c + 1] == 1:
-                       perimeter -= 1
+                        perimeter -= 1
     return perimeter
